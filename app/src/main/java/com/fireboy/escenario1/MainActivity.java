7a609cfe2847925f964ce0cf12a0b86803ed2a84
hugomartinromero <martin.romero.hugo@iescamas.es>
2024-10-09 15:25:46 +0200

    String.xml actualizado

--- app/src/main/java/com/fireboy/escenario1/MainActivity.java
+++ app/src/main/java/com/fireboy/escenario1/MainActivity.java
@@ -31,21 +31,21 @@ public class MainActivity extends AppCompatActivity {
         opciones = new ArrayList<>();
 
         if (tipo == R.id.rbCiudades) {
-            opciones.add("Sevilla");
-            opciones.add("Huelva");
-            opciones.add("Cádiz");
+            opciones.add(getString(R.string.sevilla));
+            opciones.add(getString(R.string.huelva));
+            opciones.add(getString(R.string.cadiz));
         }
 
         if (tipo == R.id.rbFrutas) {
-            opciones.add("Manzana");
-            opciones.add("Naranja");
-            opciones.add("Piña");
+            opciones.add(getString(R.string.manzana));
+            opciones.add(getString(R.string.naranja));
+            opciones.add(getString(R.string.pinia));
         }
 
         if (tipo == R.id.rbCoches) {
-            opciones.add("Seat");
-            opciones.add("Renault");
-            opciones.add("Ford");
+            opciones.add(getString(R.string.seat));
+            opciones.add(getString(R.string.renault));
+            opciones.add(getString(R.string.ford));
         }
 
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, opciones);
